b926853e3e6db6d0f58d6a3b0be92540b3e7294e
Facundo Gorosito <111923454+facundogorosito@users.noreply.github.com>
2023-11-14 18:03:38 -0300

    agrego java

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ function mostrarOcultarMenu(){
         menuVisible = true;
     }
 }
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlycss");
+        habilidades[2].classList.add("react");
+        habilidades[3].classList.add("comunica");
+        habilidades[4].classList.add("teamwork");
+        habilidades[5].classList.add("crea");
+        habilidades[6].classList.add("dedicacion");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("proyect");
+    }
+}
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
